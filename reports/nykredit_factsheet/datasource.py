944a574549f82c70dc2fe2e5ff3c056bc3466e9d
deu <deu@capital-four.com>
2025-02-07 12:54:36 +0100

    fix: cleaning up reports and metadata

--- reports/nykredit_factsheet/datasource.py
+++
@@ -1,7 +0,0 @@
-from os.path import abspath, dirname
-
-
-def curate_data(fund_name: str):
-    factsheet = dirname(abspath(__file__)) + f'\\utils\\{fund_name}.png'
-
-    return {'factsheet': factsheet}
